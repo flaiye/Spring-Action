41930d425b1eb316564095d1c2d1950bf1c161c5
flaiye <flaiye@gmail.com>
2022-11-10 11:33:25 +0300

    test

---
+++ src/main/java/com/tacocloudproj/TacoCloudProject/TacoCloudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tacocloudproj.TacoCloudProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tacocloudproj/TacoCloudProject/TacoCloudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacocloudproj.TacoCloudProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
