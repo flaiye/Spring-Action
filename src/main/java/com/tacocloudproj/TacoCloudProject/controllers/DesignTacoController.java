eddae04582ccef0fdd34cb38d56098ae399d39b5
azadurnii <azadurnii@neoflex.ru>
2022-11-11 08:21:38 +0300

    added validations

--- src/main/java/com/tacocloudproj/TacoCloudProject/controllers/DesignTacoController.java
+++ src/main/java/com/tacocloudproj/TacoCloudProject/controllers/DesignTacoController.java
@@ -7,8 +7,10 @@ import com.tacocloudproj.TacoCloudProject.model.Type;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -57,7 +59,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()){
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Process taco: {}", taco);
 
